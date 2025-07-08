dc619fb9aa419d79cac8ca4dd2fd83e731b03cee
matheus-hdas <matheussantos.betim@gmail.com>
2025-07-08 00:20:31 -0300

    chore: add `services` scripts

---
+++ up-infra/scripts/wait-for-database.js
@@ -0,0 +1,17 @@
+const { exec } = require('node:child_process');
+
+function checkPostgres() {
+  exec('docker exec postgres-dev pg_isready --host localhost', handleReturn);
+
+  function handleReturn(error, stdout) {
+    if (stdout.search('accepting connections') === -1) {
+      process.stdout.write('.');
+      checkPostgres();
+      return;
+    }
+    console.log('\n🟢 Postgres is ready and accepting connections');
+  }
+}
+
+process.stdout.write('\n🔴 Waiting for the Database to accept connections');
+checkPostgres();
