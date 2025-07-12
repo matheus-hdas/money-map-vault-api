a9936f4a104e1807a4bbacd0b2a2d7b14d97441e
matheus-hdas <matheussantos.betim@gmail.com>
2025-07-12 14:14:33 -0300

    feat: make application use filters and interceptors

--- src/main.ts
+++ src/main.ts
@@ -1,13 +1,17 @@
-import dotenv from 'dotenv';
+import { config } from 'dotenv';
 import { NestFactory } from '@nestjs/core';
 import { AppModule } from './app.module';
+import { GlobalExceptionFilter } from './common/filters/http-exception.filter';
+import { ResponseInterceptor } from './common/interceptors/response.interceptor';
 
 if (process.env.NODE_ENV === 'development' || process.env.NODE_ENV === 'test') {
-  dotenv.config({ path: '.env.development' });
+  config({ path: '.env.development' });
 }
 
 async function bootstrap() {
   const app = await NestFactory.create(AppModule);
+  app.useGlobalFilters(new GlobalExceptionFilter());
+  app.useGlobalInterceptors(new ResponseInterceptor());
   await app.listen(process.env.PORT ?? 3000);
 }
 
