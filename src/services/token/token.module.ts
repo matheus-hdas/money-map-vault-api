a7d5e239cb4da8bdc618340069b65b4152f1e9da
matheus-hdas <matheussantos.betim@gmail.com>
2025-07-14 19:48:54 -0300

    feat: add `token` module to manage jwt

---
+++ src/services/token/token.module.ts
@@ -0,0 +1,24 @@
+import { Module } from '@nestjs/common';
+import { JwtModule } from '@nestjs/jwt';
+import { ConfigService } from '@nestjs/config';
+import { TokenService } from './token.service';
+import { TypeOrmModule } from '@nestjs/typeorm';
+import { User } from 'src/modules/database/entities/user.entity';
+
+@Module({
+  imports: [
+    JwtModule.registerAsync({
+      useFactory: (configService: ConfigService) => ({
+        secret: configService.get<string>('JWT_SECRET'),
+        signOptions: {
+          expiresIn: configService.get<number>('JWT_EXPIRATION'),
+        },
+      }),
+      inject: [ConfigService],
+    }),
+    TypeOrmModule.forFeature([User]),
+  ],
+  providers: [TokenService],
+  exports: [TokenService],
+})
+export class TokenModule {}
---
+++ src/services/token/token.service.ts
@@ -0,0 +1,95 @@
+import { Injectable, UnauthorizedException } from '@nestjs/common';
+import { JwtService } from '@nestjs/jwt';
+import { User } from '../../modules/database/entities/user.entity';
+import { Repository } from 'typeorm';
+import { InjectRepository } from '@nestjs/typeorm';
+
+type TokenPayload = {
+  sub: string;
+  currency?: string;
+  iss: string;
+  type?: string;
+};
+
+type DecodedTokenPayload = TokenPayload & {
+  iat: number;
+  exp: number;
+};
+
+export type TokenObject = {
+  accessToken: string;
+  refreshToken: string;
+  expiration: number;
+};
+
+@Injectable()
+export class TokenService {
+  constructor(
+    private readonly jwtService: JwtService,
+    @InjectRepository(User)
+    private readonly userRepository: Repository<User>,
+  ) {}
+
+  generateAuthorizationToken(user: User): TokenObject {
+    const accessTokenExpiresInSeconds = 3600;
+    const tokenExpiration = new Date();
+    tokenExpiration.setSeconds(
+      tokenExpiration.getSeconds() + accessTokenExpiresInSeconds,
+    );
+
+    const accessTokenPayload: TokenPayload = {
+      sub: user.username,
+      iss: 'moneymapvault-api',
+      currency: user.defaultCurrency,
+    };
+
+    const refreshTokenPayload: TokenPayload = {
+      sub: user.username,
+      iss: 'moneymapvault-api',
+      type: 'refresh',
+    };
+
+    const accessToken = this.getAccessToken(accessTokenPayload);
+    const refreshToken = this.getRefreshToken(refreshTokenPayload);
+
+    return {
+      accessToken,
+      refreshToken,
+      expiration: tokenExpiration.getTime(),
+    };
+  }
+
+  async refreshAuthorizationToken(token: string): Promise<TokenObject> {
+    const decoded: DecodedTokenPayload = this.jwtService.verify(token);
+
+    if (!decoded.type || decoded.type !== 'refresh') {
+      throw new UnauthorizedException('Invalid token type');
+    }
+
+    const user = await this.userRepository.findOne({
+      where: { username: decoded.sub },
+    });
+
+    if (!user) {
+      throw new UnauthorizedException('Invalid token user');
+    }
+
+    return this.generateAuthorizationToken(user);
+  }
+
+  verifyToken(token: string): DecodedTokenPayload {
+    return this.jwtService.verify(token);
+  }
+
+  private getAccessToken(payload: TokenPayload) {
+    return this.jwtService.sign(payload);
+  }
+
+  private getRefreshToken(payload: TokenPayload) {
+    const refreshExpiresInSeconds = 86400;
+
+    return this.jwtService.sign(payload, {
+      expiresIn: refreshExpiresInSeconds,
+    });
+  }
+}
